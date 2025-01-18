08e1133f99ccc1b097a3d1d5b4861d2ef041ccad
Daramola Kehinde Joseph <81877552+Kennypapa@users.noreply.github.com>
2025-01-18 23:31:45 +0100

    update on skeleton loader

--- src/views/posts/FYP.tsx
+++ src/views/posts/FYP.tsx
@@ -4,12 +4,14 @@ import { initFlowbite } from "flowbite";
 import Avatar from '../../assets/images/avatar1.png';
 import { useEffect } from "react";
 import { toast } from 'react-toastify';
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 
-const FYP = ()=> { 
+const FYP = () => {
 
     return (
-        <ul className="space-y-5 w-full flex justify-between items-start flex-col">
+        <ul className="space-y-5 w-full flex justify-between items-start flex-col h-[900px] overflow-y-scroll">
 
             <li className="w-full">
                 <div className=" w-[600px] post_box mx-auto dark:bg-darkFaintGray bg-[#F9F9F9] rounded-[8px] pt-5">
@@ -186,6 +188,46 @@ const FYP = ()=> {
                                     </svg>
                                 </span>
                             </li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+
+            <li className="w-full">
+
+                <div className="w-[600px] post_box mx-auto dark:bg-darkFaintGray bg-[#F9F9F9] rounded-[8px] pt-5">
+
+                    <div className="flex justify-between items-start pb-2 px-4">
+                        <div className="flex gap-3">
+                            <Skeleton circle={true} width={54} height={54} />
+                            <div>
+                                <Skeleton width={120} height={16} style={{ marginBottom: "0.5rem" }} />
+                                <Skeleton width={80} height={12} style={{ marginBottom: "0.3rem" }} />
+                                <Skeleton width={60} height={12} />
+                            </div>
+                        </div>
+                        <Skeleton circle={true} width={24} height={24} />
+                    </div>
+
+                    <div className="px-4">
+                        <Skeleton width="100%" height={16} style={{ marginBottom: "0.5rem" }} />
+                        <Skeleton width="75%" height={16} />
+                    </div>
+
+                    <div className="sm:h-[320px] h-[220px] mt-4">
+                        <Skeleton width="100%" height="100%" />
+                    </div>
+
+                    <div id="icons" className="py-5 px-12">
+                        <ul className="flex justify-between">
+                            {[1, 2, 3, 4].map((index) => (
+                                <li key={index}>
+                                    <div className="flex justify-center items-center gap-2">
+                                        <Skeleton circle={true} width={24} height={24} />
+                                        <Skeleton width={40} height={16} />
+                                    </div>
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </div>
