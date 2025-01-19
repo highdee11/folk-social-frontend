d3975751166d0990fabbd88c07c32610d64f0df9
Daramola Kehinde Joseph <81877552+Kennypapa@users.noreply.github.com>
2025-01-19 22:46:13 +0100

    updates

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,11 +1,12 @@
+import Avatar from '../assets/images/highdee.ai.jpeg';
 const Profile = () => {
     return (
-        <div className=" relative lg:w-auto w-[98%] mx-auto bg-white mt-12 rounded-[12px] pt-5 px-8 flex justify-between items-center">
+        <div className=" relative lg:w-auto w-[98%] mx-auto bg-white mt-12 rounded-[12px] py-5 px-8 flex justify-between items-center shadow-sm profileBg">
 
             <div className=" flex justify-start items-center gap-6">
 
-                <div className=" w-[190px] h-[190px] border rounded-full">
-
+                <div className=" w-[190px] h-[190px] bg-[#B190B6] border rounded-full">
+                    <img src={Avatar} className=' rounded-full' />
                 </div>
 
                 <div>
@@ -14,9 +15,24 @@ const Profile = () => {
                     </p>
 
                     <div>
-                        <ul>
+                        <ul className="flex gap-4">
                             <li>
-
+                                <p className=" text-base flex gap-1">
+                                    <span className='text-darkColor OnestMedium'>2563</span>
+                                    <span>Following</span>
+                                </p>
+                            </li>
+                            <li>
+                                <p className=" text-base flex gap-1">
+                                    <span className='text-darkColor OnestMedium'>2563</span>
+                                    <span>Followers</span>
+                                </p>
+                            </li>
+                            <li>
+                                <p className=" text-base flex gap-1">
+                                    <span className='text-darkColor OnestMedium'>2563</span>
+                                    <span>Following</span>
+                                </p>
                             </li>
                         </ul>
                     </div>
