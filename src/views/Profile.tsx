c82b278e2c0593487ee22dff950c1e21034aed5c
Daramola Kehinde Joseph <81877552+Kennypapa@users.noreply.github.com>
2025-01-20 11:45:44 +0100

    updates

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,37 +1,37 @@
 import Avatar from '../assets/images/highdee.ai.jpeg';
 const Profile = () => {
     return (
-        <div className=" relative lg:w-auto w-[98%] mx-auto bg-white mt-12 rounded-[12px] py-5 px-8 flex justify-between items-center shadow-sm profileBg">
+        <div className=" relative lg:w-auto w-[98%] mx-auto bg-darkColor mt-12 rounded-[12px] py-5 px-8 flex justify-between items-center shadow-sm profileBg">
 
             <div className=" flex justify-start items-center gap-6">
 
-                <div className=" w-[190px] h-[190px] bg-[#B190B6] border rounded-full">
+                <div className=" w-[190px] h-[190px] bg-[#B190B6]  rounded-full">
                     <img src={Avatar} className=' rounded-full' />
                 </div>
 
                 <div>
-                    <p>
+                    <p className=' text-white dark:text-darkColor'>
                         highdee.ai
                     </p>
 
                     <div>
                         <ul className="flex gap-4">
                             <li>
-                                <p className=" text-base flex gap-1">
-                                    <span className='text-darkColor OnestMedium'>2563</span>
-                                    <span>Following</span>
+                                <p className=" text-base flex justify-start items-center gap-1">
+                                    <span className='dark:text-darkColor text-white OnestMedium'>2563</span>
+                                    <span className='dark:text-darkColor text-white text-sm'>Following</span>
                                 </p>
                             </li>
                             <li>
                                 <p className=" text-base flex gap-1">
-                                    <span className='text-darkColor OnestMedium'>2563</span>
-                                    <span>Followers</span>
+                                    <span className='dark:text-darkColor text-white OnestMedium'>2563</span>
+                                    <span className='dark:text-darkColor text-white text-sm'>Followers</span>
                                 </p>
                             </li>
                             <li>
                                 <p className=" text-base flex gap-1">
-                                    <span className='text-darkColor OnestMedium'>2563</span>
-                                    <span>Following</span>
+                                    <span className='dark:text-darkColor text-white OnestMedium'>2563</span>
+                                    <span className='dark:text-darkColor text-white text-sm'>Following</span>
                                 </p>
                             </li>
                         </ul>
@@ -42,7 +42,7 @@ const Profile = () => {
 
 
             <div>
-                <button className="px-5 py-2 bg-darkColor rounded-[8px]  text-white">
+                <button className="px-5 py-2 bg-white rounded-[8px]  text-darkColor">
                     Edit Profile
                 </button>
             </div>
